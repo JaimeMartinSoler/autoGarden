f53e427d8b583fb92592fe249a606036f3b605ce
Jaime <jaime.martin.soler@gmail.com>
2016-11-11 16:32:09 +0100

    .

--- src/RPi/twitterActionManager.py
+++ src/RPi/twitterActionManager.py
@@ -43,15 +43,16 @@ def txNormalActionManager():
 	TEMP_AIR_period = 30.0							# TEMP_AIR_period in minutes
 	TEMP_AIR_period_j = TEMP_AIR_period/(1440.0)	# TEMP_AIR_period in julian
 	TEMP_AIR_last_j = 0.0
-  # parameters: twitter
-  twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-  
+	# parameters: twitter
+	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	
 	# setup: create a fake (txSuccess=1) to avoid tx and jump wait loop
 	txTwitterAction.txSuccess = 1
 	txTwitterAction.txReadyToTx = True
 	
 	# loop: check timers and twitter, updating txTwitterAction if so
 	while (PROCESS.isAlive):
+		
 		# wait for previous action to be transmitted
 		while(txTwitterAction.txReadyToTx and txTwitterAction.txSuccess<=0):
 			# Check PROCESS.isAlive
@@ -59,21 +60,24 @@ def txNormalActionManager():
 				return
 			time.sleep(0.100)
 		nowJulianDT = nowJulian()
+		
 		# TEMP_AIR management
 		if ((nowJulianDT - TEMP_AIR_last_j) >= TEMP_AIR_period_j):
-      # get last TEMP_AIR temperature from DB
-      DBcursor.execute('SELECT * FROM WEATHER WHERE WPAR=\'TEMP\' AND WPARID=\'AIR\' ORDER BY DATETIME DESC LIMIT 1;')
-	    DBrow = DBcursor.fetchone()
-      # make a tweet about the TEMP_AIR
-	    if (DBrow is not None):
-        TEMP_AIR_last_j = datetimeToJulian(DBrow[1])
-		    TEMP_AIR_value = DBrow[6]
-        tweetText = "Oh dear, the temperature is {:.1f} celsius, that is so grateful!".format(TEMP_AIR_value)
-        try:
-          twitter.update_status(status=tweetText)
-        except TwythonError as e:
-          LOG(LOG_ERR,"<<< ERROR: TwythonError: \"{}\" >>>".format(e))
-      else:
-        LOG(LOG_WAR,"<<< WARNING: No rows found in DB (txTwitterActionManager, WEATHER) >>>")
+			# get last TEMP_AIR temperature from DB
+			DBcursor.execute('SELECT * FROM WEATHER WHERE WPAR=\'TEMP\' AND WPARID=\'AIR\' ORDER BY DATETIME DESC LIMIT 1;')
+			DBrow = DBcursor.fetchone()
+			# make a tweet about the TEMP_AIR
+			if (DBrow is not None):
+			TEMP_AIR_last_j = datetimeToJulian(DBrow[1])
+			TEMP_AIR_value = DBrow[6]
+			tweetText = "Oh dear, the temperature is {:.1f} celsius, that is so grateful!".format(TEMP_AIR_value)
+			try:
+				twitter.update_status(status=tweetText)
+			except TwythonError as e:
+				LOG(LOG_ERR,"<<< ERROR: TwythonError: \"{}\" >>>".format(e))
+		else:
+			LOG(LOG_WAR,"<<< WARNING: No rows found in DB (txTwitterActionManager, WEATHER) >>>")
+			
+		# delay
 		time.sleep(1.0)
-		
+
